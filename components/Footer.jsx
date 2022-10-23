c536e61a5c1417431aa4cc7d9e5c2800f347a857
s92i <sofiane.92izii@gmail.com>
2022-10-23 19:02:32 +0100

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react';
+
+const Footer = () => (
+    <Box textAlign="center" p="5" color="grey.600" borderTop="1px" borderColor="grey.100">
+        2021 Real Estate, Inc.
+    </Box>
+)
+
+export default Footer;
